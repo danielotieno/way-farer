13e6ff7ddff57cf75439283ab6c3a60999a44da9
Daniel Otieno <danielotieno.ke@gmail.com>
2019-07-12 22:20:38 +0300

    [chore #167253011] Add js modal popup

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,1 +1,19 @@
 document.querySelector("#today").valueAsDate = new Date();
+
+let modal = document.querySelector(".modal");
+let trigger = document.querySelector(".myButton");
+let closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
